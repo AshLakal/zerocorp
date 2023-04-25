574be18488a975794f7bac636808f9b4735bce2a
AshLakal <131367445+AshLakal@users.noreply.github.com>
2023-04-25 09:23:34 +0200

    Update Mobile

--- zerocorp.js
+++ zerocorp.js
@@ -1,17 +1,10 @@
+
 // Disable browser's default scroll restoration behavior
 if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual';
 }
 
-// Function to disable scrolling
-function disableScroll() {
-    document.body.classList.add('no-scroll');
-}
-
-// Function to enable scrolling
-function enableScroll() {
-    document.body.classList.remove('no-scroll');
-}
+let isStartingAnimation = true;
 
 // Function to reset scroll position and retrigger animation
 function resetScrollAndAnimation() {
@@ -19,59 +12,50 @@ function resetScrollAndAnimation() {
     window.scrollTo(0, 0);
 
     // Disable scrolling and retrigger animation
-    disableScroll();
+    isStartingAnimation = true;
 
     // Enable scrolling after the animation ends
     setTimeout(function () {
-        enableScroll();
+        isStartingAnimation = false;
+        window.scrollTo(0, 0);
     }, 1000); // Replace 5000 with the duration of your starting animation in milliseconds
 }
 
 // Reset scroll position and retrigger animation on page load
 resetScrollAndAnimation();
 
-
 function animateMobileElements() {
 
     const animations = [
-        {
-            element: document.querySelector('.zerocorp'),
-            keyframes: [{ opacity: 0 }, { opacity: 0.15 }],
-            options: { duration: 1000, easing: 'ease-out' }
-        },
+        // ...
         {
             element: document.querySelector('#immeubleA'),
-            keyframes: [{ left: '26%' }, { left: '23%' }],
+            keyframes: [{ left: '26vw' }, { left: '23vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('#immeubleC'),
-            keyframes: [{ left: '10%' }, { left: '7%' }],
+            keyframes: [{ left: '10vw' }, { left: '7vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('#immeubleB'),
-            keyframes: [{ right: '13%' }, { right: '10%' }],
+            keyframes: [{ right: '13vw' }, { right: '10vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('#immeubleD'),
-            keyframes: [{ right: '21%' }, { right: '18%' }],
-            options: { duration: 1000, easing: 'ease-out' }
-        },         
-        {
-            element: document.querySelector('.floating-title'),
-            keyframes: [{ transform: 'translateX(100%)', opacity: 0 }, { transform: 'translateX(0)', opacity: 1 }],
+            keyframes: [{ right: '21vw' }, { right: '18vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('.cave'),
-            keyframes: [{ bottom: '-111%' }, { bottom: '-110%' }],
+            keyframes: [{ bottom: '-111vh' }, { bottom: '-110vh' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('.custom-element'),
-            keyframes: [{ bottom: '5%' }, { bottom: '8%' }],
+            keyframes: [{ bottom: '5vh' }, { bottom: '8vh' }],
             options: { duration: 1000, easing: 'ease-out' }
         }
     ];
@@ -98,47 +82,38 @@ function animateElements() {
     const animations = [
         {
             element: document.querySelector('#newLayer'),
-            keyframes: [{ bottom: '0%' }, { bottom: '-2%' }],
+            keyframes: [{ bottom: '0vh' }, { bottom: '-2vh' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
-        {
-            element: document.querySelector('.zerocorp'),
-            keyframes: [{ opacity: 0 }, { opacity: 0.15 }],
-            options: { duration: 1000, easing: 'ease-out' }
-        },
+        // ...
         {
             element: document.querySelector('#immeubleA'),
-            keyframes: [{ left: '21%' }, { left: '18%' }],
+            keyframes: [{ left: '21vw' }, { left: '18vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('#immeubleC'),
-            keyframes: [{ left: '32%' }, { left: '30%' }],
+            keyframes: [{ left: '32vw' }, { left: '30vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('#immeubleB'),
-            keyframes: [{ right: '15%' }, { right: '12%' }],
+            keyframes: [{ right: '15vw' }, { right: '12vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('#immeubleD'),
-            keyframes: [{ right: '22%' }, { right: '20%' }],
-            options: { duration: 1000, easing: 'ease-out' }
-        },         
-        {
-            element: document.querySelector('.floating-title'),
-            keyframes: [{ transform: 'translateX(100%)', opacity: 0 }, { transform: 'translateX(0)', opacity: 1 }],
+            keyframes: [{ right: '22vw' }, { right: '20vw' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('.cave'),
-            keyframes: [{ bottom: '-113%' }, { bottom: '-110%' }],
+            keyframes: [{ bottom: '-113vh' }, { bottom: '-110vh' }],
             options: { duration: 1000, easing: 'ease-out' }
         },
         {
             element: document.querySelector('.custom-element'),
-            keyframes: [{ bottom: '5%' }, { bottom: '8%' }],
+            keyframes: [{ bottom: '5vh' }, { bottom: '8vh' }],
             options: { duration: 1000, easing: 'ease-out' }
         }
     ];
@@ -171,65 +146,70 @@ function animateElements() {
     
 
 
-function updateAnimation() {
-    const scrollPos = window.pageYOffset;
-    const maxScroll = document.body.scrollHeight - window.innerHeight;
-    const scrollPercent = scrollPos / maxScroll;
-
-    const floatingTitle = document.querySelector('.floating-title');
-    const titleProgress = scrollPercent * 200;
-    floatingTitle.style.transform = `translateX(${-titleProgress}%)`;
-    floatingTitle.style.opacity = 1 - scrollPercent;
-
-    // Animate the .cave element
-    const cave = document.querySelector('.cave');
-    const caveBottom = -110 + scrollPercent * 110; // Change the initial value and multiplier
-    cave.style.bottom = `${caveBottom}%`;
-
-    if (window.innerWidth <= 767) {
-        document.querySelector('.zerocorp').style.opacity = 0.15 + scrollPercent * 0.55;
-        document.querySelector('#immeubleA').style.left = `${23 - scrollPercent * 8}%`;
-        document.querySelector('#immeubleC').style.left = `${7 - scrollPercent * 3.4}%`;
-        document.querySelector('#immeubleB').style.right = `${10 - scrollPercent * 11.66}%`;
-        document.querySelector('#immeubleD').style.right = `${18 - scrollPercent * 6.33}%`;
-        document.querySelector('#newLayer').style.bottom = `${7 - scrollPercent * -70}%`;
-        document.querySelector('.cables').style.right = `-${70 - scrollPercent * 50}%`;
-        document.querySelector('.custom-element').style.bottom = `${8 - scrollPercent * -110}%`;   
-    } else {  
-    document.querySelector('.zerocorp').style.opacity = 0.15 + scrollPercent * 0.55;
-    document.querySelector('#immeubleA').style.left = `${18 - scrollPercent * 20}%`;
-    document.querySelector('#immeubleC').style.left = `${30 - scrollPercent * 15}%`;
-    document.querySelector('#immeubleB').style.right = `${12 - scrollPercent * 11}%`;
-    document.querySelector('#immeubleD').style.right = `${20 - scrollPercent * 15}%`;
-    document.querySelector('#newLayer').style.bottom = `${-2 - scrollPercent * 5}%`;
-    document.querySelector('.cables').style.right = `-${70 - scrollPercent * 50}%`;
-    document.querySelector('.custom-element').style.bottom = `${8 - scrollPercent * -110}%`;
+    function updateAnimation() {
+        if (isStartingAnimation) {
+            return;
+        }
+        const scrollPos = window.pageYOffset;
+        const maxScroll = document.body.scrollHeight - window.innerHeight;
+        const scrollPercent = scrollPos / maxScroll;
+    
+        const floatingTitle = document.querySelector('.floating-title');
+        const titleProgress = scrollPercent * 200;
+        floatingTitle.style.transform = `translateX(${-titleProgress}%)`;
+        floatingTitle.style.opacity = 1 - scrollPercent;
+    
+        // Animate the .cave element
+        const cave = document.querySelector('.cave');
+        const caveBottom = -110 + scrollPercent * 110; // Change the initial value and multiplier
+        cave.style.bottom = `${caveBottom}%`;
+    
+        if (window.innerWidth <= 767) {
+            document.querySelector('.zerocorp').style.opacity = 0.15 + scrollPercent * 0.55;
+            document.querySelector('#immeubleA').style.left = `calc(23vw - ${scrollPercent * 8}vw)`;
+            document.querySelector('#immeubleC').style.left = `calc(7vw - ${scrollPercent * 3.4}vw)`;
+            document.querySelector('#immeubleB').style.right = `calc(10vw - ${scrollPercent * 11.66}vw)`;
+            document.querySelector('#immeubleD').style.right = `calc(18vw - ${scrollPercent * 6.33}vw)`;
+            document.querySelector('#newLayer').style.bottom = `calc(7vh - ${scrollPercent * -70}vh)`;
+            document.querySelector('.cables').style.right = `calc(-${70 - scrollPercent * 50}vw)`;
+            document.querySelector('.custom-element').style.bottom = `calc(8vh - ${scrollPercent * -110}vh)`;   
+        } else {  
+            document.querySelector('.zerocorp').style.opacity = 0.15 + scrollPercent * 0.55;
+            document.querySelector('#immeubleA').style.left = `calc(18vw - ${scrollPercent * 20}vw)`;
+            document.querySelector('#immeubleC').style.left = `calc(30vw - ${scrollPercent * 15}vw)`;
+            document.querySelector('#immeubleB').style.right = `calc(12vw - ${scrollPercent * 11}vw)`;
+            document.querySelector('#immeubleD').style.right = `calc(20vw - ${scrollPercent * 15}vw)`;
+            document.querySelector('#newLayer').style.bottom = `calc(-2vh - ${scrollPercent * 5}vh)`;
+            document.querySelector('.cables').style.right = `calc(-${70 - scrollPercent * 50}vw)`;
+            document.querySelector('.custom-element').style.bottom = `calc(8vh - ${scrollPercent * -110}vh)`;
+        }
+        if (window.innerWidth <= 767) {
+            document.querySelector('#immeubleA').style.bottom = `calc(7vh - ${scrollPercent * -105}vh)`;
+            document.querySelector('#immeubleC').style.bottom = `calc(7vh - ${scrollPercent * -80}vh)`;
+            document.querySelector('#immeubleB').style.bottom = `calc(7vh - ${scrollPercent * -105}vh)`;
+            document.querySelector('#immeubleD').style.bottom = `calc(7vh - ${scrollPercent * -80}vh)`;
+        } else { 
+            document.querySelector('#immeubleA').style.bottom = `calc(5vh - ${scrollPercent * -40}vh)`;
+            document.querySelector('#immeubleC').style.bottom = `calc(5vh - ${scrollPercent * -12}vh)`;
+            document.querySelector('#immeubleB').style.bottom = `calc(5vh - ${scrollPercent * -40}vh)`;
+            document.querySelector('#immeubleD').style.bottom = `calc(5vh - ${scrollPercent * -12}vh)`;
+        }
     }
-    if (window.innerWidth <= 767) {
-    document.querySelector('#immeubleA').style.bottom = `${7 - scrollPercent * -105}%`;
-    document.querySelector('#immeubleC').style.bottom = `${7 - scrollPercent * -80}%`;
-    document.querySelector('#immeubleB').style.bottom = `${7 - scrollPercent * -105}%`;
-    document.querySelector('#immeubleD').style.bottom = `${7 - scrollPercent * -80}%`;
-    } else { 
-        document.querySelector('#immeubleA').style.bottom = `${5 - scrollPercent * -40}%`;
-        document.querySelector('#immeubleC').style.bottom = `${5 - scrollPercent * -12}%`;
-        document.querySelector('#immeubleB').style.bottom = `${5 - scrollPercent * -40}%`;
-        document.querySelector('#immeubleD').style.bottom = `${5 - scrollPercent * -12}%`;
-}
-}
-document.addEventListener('scroll', updateAnimation);
+    
+    document.addEventListener('scroll', updateAnimation);
   
 
 let animationPlayed = false;
 
 window.addEventListener("scroll", () => {
-    if (!animationPlayed && window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    if (!animationPlayed && !isStartingAnimation && window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
         animationPlayed = true;
         setTimeout(() => {
             animateOrdi();
         }, 500);
     }
 });
+
 
 function animateOrdi() {
     const ordi = document.querySelector(".ordi");
